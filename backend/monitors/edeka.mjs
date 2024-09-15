3897089afcc397d21dc0eb15d127d6dd2588009c
Tobias Thiemann <tobithieman@gmail.com>
2024-09-16 00:47:57 +0200

    implement stores edeka

--- backend/monitors/edeka.mjs
+++ backend/monitors/edeka.mjs
@@ -1,11 +1,11 @@
 import Store from "../schemas/storeSchema.mjs"
-import { insertOffer, setLastFetched } from "../util/database.mjs"
+import { insertOffer, insertStore, setLastFetched } from "../util/database.mjs"
 import { allowedToFetch } from "../util/dates.mjs"
 
 export async function importEdeka() {
     const monitorName = "edeka"
 
-    if(!await allowedToFetch(monitorName)) {
+    if (!await allowedToFetch(monitorName)) {
         return
     }
 
@@ -18,36 +18,20 @@ export async function importEdeka() {
         return
     }
 
-    //let storesPagesCount = Math.ceil(((await (await fetch("https://www.edeka.de/api/marketsearch/markets")).json())["totalCount"] / 999))
+    let storesPagesCount = Math.ceil(((await (await fetch("https://www.edeka.de/api/marketsearch/markets")).json())["totalCount"] / 999))
 
-    //Array.from(Array(storesPagesCount).keys()).forEach(async (number) => {
-//
-    //    let result = await (await fetch(`https://www.edeka.de/api/marketsearch/markets?size=999&page=${number}`)).json()
-//
-    //    result["markets"].forEach(async (store) => {
-    //        const options = {
-    //            upsert: true,
-    //            new: true,
-    //            setDefaultsOnInsert: true
-    //        }
-//
-    //        const filterOffer = {
-    //            name: store["name"],
-    //            group: "edeka",
-    //            targetApiIdentifier: store["id"]
-    //        }
-//
-    //        const st = await Store.findOneAndUpdate(filterOffer, filterOffer, options)
-//
-    //        try {
-    //            await st.save()
-    //        } catch (error) {
-    //            console.log(`error import: ${store["name"]} - edeka - ${store["id"]}`)
-    //            res.sendStatus(400)
-    //            return
-    //        }
-    //    })
-    //})
+    Array.from(Array(storesPagesCount).keys()).forEach(async (number) => {
+
+        let result = await (await fetch(`https://www.edeka.de/api/marketsearch/markets?size=999&page=${number}`)).json()
+
+        result["markets"].forEach(async (store) => {
+            insertStore({
+                group: "edeka",
+                targetApiIdentifier: store["id"],
+                data: store
+            })
+        })
+    })
 
     let startDateTime = new Date(offers["validFrom"])
     let endDateTime = new Date(offers["validTill"])
